8c8f4c40da9b9aef0e8979ee058f09934bc7a316
Saule <saulegri@gmail.com>
2023-03-08 16:03:57 +0200

    commit

--- src/com/itacademy/eshop/services/ShopService.java
+++ src/com/itacademy/eshop/services/ShopService.java
@@ -1,6 +1,7 @@
 package com.itacademy.eshop.services;
 
 import com.itacademy.eshop.product.Product;
+import com.itacademy.eshop.product.types.Category;
 import com.itacademy.eshop.shop.Eshop;
 
 import java.util.ArrayList;
@@ -9,21 +10,26 @@ public class ShopService {
     /*** This class is used for creating new e-shop object and initializing it with some products.
      */
 
+
     public Eshop createShop() {
         /** Should create new e-shop object and initialize it with some products.
          * Should return e-shop object.
          */
+
        ArrayList<Product> products = createProducts();
-        throw new UnsupportedOperationException("createShop() is not implemented yet");
+       return new Eshop("sauleshop", products);
     }
 
     private ArrayList<Product> createProducts() {
-        /**
-         * Should return a list of products. Should contain at least 6 products,
-         * one per each category in Category enum.
-         * Product should not have any reviews.
-         */
-        throw new UnsupportedOperationException("createProducts() is not implemented yet");
+        ArrayList<Product>arrayList = new ArrayList<>();
+        arrayList.add(new Product("shirt",78,Category.CLOTHING));
+        arrayList.add(new Product("butter",2,Category.FOOD));
+        arrayList.add(new Product("saule",12,Category.BOOKS));
+        arrayList.add(new Product("phone",1000,Category.ELECTRONICS));
+        arrayList.add(new Product("lego",99,Category.TOYS));
+        arrayList.add(new Product("milk",2,Category.FOOD));
+        return arrayList;
+
     }
 
 }

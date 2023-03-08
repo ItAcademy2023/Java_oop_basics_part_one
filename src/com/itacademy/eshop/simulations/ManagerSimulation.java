b71b1488ae2bb7d1c061f4ea9415ab8359e5133e
Ignas <ignasbieksa@gmail.com>
2023-03-08 16:14:52 +0200

    Task #2 Finished ManagerSimulation

--- src/com/itacademy/eshop/simulations/ManagerSimulation.java
+++ src/com/itacademy/eshop/simulations/ManagerSimulation.java
@@ -1,6 +1,7 @@
 package com.itacademy.eshop.simulations;
 
 import com.itacademy.eshop.product.Product;
+import com.itacademy.eshop.product.Review;
 import com.itacademy.eshop.product.types.Category;
 import com.itacademy.eshop.shop.Eshop;
 
@@ -28,24 +29,32 @@ public class ManagerSimulation {
          * adds three new products to the shop. One of them should be a book, one should be a Laptop, and one should be a shirt.
          */
         Product laptop = new Product("Laptop", 1000, Category.ELECTRONICS);
+        Product book = new Product("Book", 1000, Category.BOOKS);
+        Product shirt = new Product("Shirt", 1000, Category.CLOTHING);
+        shop.addProduct(laptop);
+        shop.addProduct(book);
+        shop.addProduct(shirt);
     }
 
     private void findAndRemoveOneProduct() {
         /**
-         * manager searcher for a product with the name "Laptop" (the one added a moment before) and removes it from the shop
+         * manager searches for a product with the name "Laptop" (the one added a moment before) and removes it from the shop
          */
+        shop.removeProductByName("Laptop");
     }
 
     private void changePriceForOneProduct() {
         /**
          * manager searches for a product with the name "Shirt" (the one added a moment before) and changes its price to 20
          */
+        shop.changePriceForProduct("Shirt", 20);
     }
 
     private void removeAllProductsWithCategory(Category category) {
         /**
          * manager removes all products with the category FOOD
          */
+        shop.removeProductByCategory(category);
     }
 
 

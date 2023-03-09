0f19cf44e8ed2462b9e4882dda3c7dddadfc6df3
dziugassupinis <127023713+dziugassupinis@users.noreply.github.com>
2023-03-09 12:55:02 +0200

    Correct Extra class

--- src/com/itacademy/eshop/product/types/Extra.java
+++ src/com/itacademy/eshop/product/types/Extra.java
@@ -2,12 +2,32 @@ package com.itacademy.eshop.product.types;
 
 public class Extra {
     private String name;
+    private String lastName;
+    private Integer age;
 
-    public void setName(String n){
+    public void setName(String n, String l, Integer a){
         this.name=n;
+        this.lastName=l;
+        this.age=a;
     }
     public String getName(){
         return name;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public Integer getAge(){
+        return age;
+    }
+
+    public void setName(String s){
+        this.name=s;
+    }
+    public void setLastName(String s){
+        this.lastName=s;
+    }
+    public void setAge(Integer a){
+        this.age=a;
     }
 
     }
